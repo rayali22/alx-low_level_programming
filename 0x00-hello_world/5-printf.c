f0d956d5b7e60862b3fc739c400843d1b344698d
rayali22 <rayali0619@gmail.com>
2023-09-07 10:42:11 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: A C program that print with put function
+ * Description: A C program that print with printf function
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        puts("\"with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
