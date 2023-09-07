528fe410fc3221b04e3e38c6ba403aec8636aa95
rayali22 <rayali0619@gmail.com>
2023-09-07 11:34:43 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,8 +10,8 @@
 */
 
 int main(void)
-{	char quo[]  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+{	char quo[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, quo, 59);
 	return (1);
 }
