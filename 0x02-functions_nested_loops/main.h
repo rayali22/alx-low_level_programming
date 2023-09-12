c66e1865511147f6dc424251208ddec4f9fbec0b
rayali22 <rayali0619@gmail.com>
2023-09-12 15:52:48 -0700

    done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,12 @@
 #include <stdio.h>
 
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+void jack_bauer(void);
 
 #endif
