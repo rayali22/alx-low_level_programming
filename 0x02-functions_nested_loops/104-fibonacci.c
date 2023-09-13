ed965beb9423f31b485675a75f1b39b07cdacbd7
rayali22 <rayali0619@gmail.com>
2023-09-12 19:54:26 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -40,7 +40,7 @@ int main(void)
 	{
 		if (f1o > 0)
 			 printf("%lu", f1o);
-		initial0s = numLenght(mx) - 1 -numLenght(f1);
+		initial0s = numLenght(mx) - 1 - numLenght(f1);
 
 		while (f1o > 0 && initial0s > 0)
 		{
