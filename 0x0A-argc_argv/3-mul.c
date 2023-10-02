b2030f6e454aaf3bc308a668530a4490e6a622bc
rayali22 <rayali0619@gmail.com>
2023-10-02 12:35:21 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,30 +3,28 @@
 #include "holberton.h"
 
 /**
- *  * main - Program that takes first two integer arguments and prints the product
+ * main - Program that takes first two integer arguments and prints the product
  *   * @argc: Number of command line arguments
  *    * @argv: Array name
  *     * Return: 1 if not enough arguments passed in, 0 otherwise
- *      */
+*/
 
 int main(int argc, char *argv[])
 {
-		int i, j;
+	int i, j;
 
-			if (argc == 1 || argc == 2)
-					{
-								printf("Error\n");
-										return (1);
-											}
-				else
-						{
-									j = 1;
+		if (argc == 1 || argc == 2)
+		{
+			printf("Error\n");
+		return (1);
+		}
+		else
+		{
+			j = 1
+			for (i = 1; i < 3; i++)
+				j *= atoi(argv[i]);
+			printf("%d\n", j)
+		}
 
-											for (i = 1; i < 3; i++)
-															j *= atoi(argv[i]);
-
-													printf("%d\n", j);
-														}
-
-					return (0);
+	return (0);
 }
