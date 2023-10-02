a04913dbc1e697324b595818a642588f6ce96796
rayali22 <rayali0619@gmail.com>
2023-10-02 13:01:45 -0700

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "holberton.h"
 
 /**
- * main - program that adds positive numbers.
- * @argc: number of arguments
- * @argv: array with the arguments
- *
- * Return: always 0
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
 **/
 
 int main(int argc, char *argv[])
 {
-	int i, suma = 0, res = 0;
-	char c[] = "Error", *find_letter;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc > 1)
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
-		}
-			find_letter = argv[i];
-			while (*find_letter != 0)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				if (*find_letter < 47 || *find_letter > 57)
+				if (isdigit(*(ptr + j)) == 0)
 				{
-					printf("%s\n", c);
+					printf("Error\n");
 					return (1);
 				}
-				find_letter++;
 			}
-			res = atoi(argv[i]);
-			suma += res;
+				sum += atoi(argv[i]);
 		}
-		printf("%d\n", suma);
+		printf("%d\n", sum);
 	}
-	else
-		printf("%d\n", 0);
-	return (0);
+		return (0);
 }
